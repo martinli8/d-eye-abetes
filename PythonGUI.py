d5b2d8438996b365b836f77b328f09bf06129231
Matt Laptop <mdguptil@gmail.com>
2019-04-05 13:34:59 -0400

    dding Example code for displaying live image viewing.

---
+++ PythonGUI.py
@@ -0,0 +1,303 @@
+# coding=utf-8
+# =============================================================================
+# Copyright (c) 2001-2019 FLIR Systems, Inc. All Rights Reserved.
+#
+# This software is the confidential and proprietary information of FLIR
+# Integrated Imaging Solutions, Inc. ("Confidential Information"). You
+# shall not disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with FLIR Integrated Imaging Solutions, Inc. (FLIR).
+#
+# FLIR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+# SOFTWARE, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE, OR NON-INFRINGEMENT. FLIR SHALL NOT BE LIABLE FOR ANY DAMAGES
+# SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+# THIS SOFTWARE OR ITS DERIVATIVES.
+# =============================================================================
+#
+# This pythonGUI.py shows how to get the image data, and then display images in a GUI.
+# This example relies on information provided in the ImageChannelStatistics.py examples.
+#
+# This example demonstrates how to display an image represented as a numpy array.
+# Currently, this program is limited to single camera use
+# NOTE: matplotlib must be installed on Python interpreter prior to running this example
+
+import os
+import PySpin
+import matplotlib.pyplot as plt
+import sys
+import keyboard
+
+
+def acquire_and_display_images(cam, nodemap, nodemap_tldevice):
+    """
+    This function acquires and displays the channel statistics of N images from a device.
+
+    :param cam: Camera to acquire images from.
+    :param nodemap: Device nodemap.
+    :param nodemap_tldevice: Transport layer device nodemap.
+    :type cam: CameraPtr
+    :type nodemap: INodeMap
+    :type nodemap_tldevice: INodeMap
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    
+    sNodemap = cam.GetTLStreamNodeMap()
+
+    # Change bufferhandling mode to NewestOnly
+    node_bufferhandling_mode = PySpin.CEnumerationPtr(sNodemap.GetNode('StreamBufferHandlingMode'))
+    if not PySpin.IsAvailable(node_bufferhandling_mode) or not PySpin.IsWritable(node_bufferhandling_mode):
+        print('Unable to set streambuferhandlingmode.. Aborting...')
+        return False
+
+    # Retrieve entry node from enumeration node
+    node_newestonly = node_bufferhandling_mode.GetEntryByName('NewestOnly')
+    if not PySpin.IsAvailable(node_newestonly) or not PySpin.IsReadable(node_newestonly):
+        print('Unable to set streambuferhandlingmode.. Aborting...')
+        return False
+
+    # Retrieve integer value from entry node
+    node_newestonly_mode = node_newestonly.GetValue()
+
+    # Set integer value from entry node as new value of enumeration node
+    node_bufferhandling_mode.SetIntValue(node_newestonly_mode)
+
+    print('*** IMAGE ACQUISITION ***\n')
+    try:
+        result = True
+
+        node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+        if not PySpin.IsAvailable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
+            print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
+            return False
+
+        # Retrieve entry node from enumeration node
+        node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
+        if not PySpin.IsAvailable(node_acquisition_mode_continuous) or not PySpin.IsReadable(
+                node_acquisition_mode_continuous):
+            print('Unable to set acquisition mode to continuous (entry retrieval). Aborting...')
+            return False
+
+        # Retrieve integer value from entry node
+        acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+
+        # Set integer value from entry node as new value of enumeration node
+        node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+
+        print('Acquisition mode set to continuous...')
+
+        #  Begin acquiring images
+        #
+        #  *** NOTES ***
+        #  What happens when the camera begins acquiring images depends on the
+        #  acquisition mode. Single frame captures only a single image, multi
+        #  frame catures a set number of images, and continuous captures a
+        #  continuous stream of images. Because the example calls for the
+        #  retrieval of 10 images, continuous mode has been set.
+        #
+        #  *** LATER ***
+        #  Image acquisition must be ended when no more images are needed.
+        cam.BeginAcquisition()
+
+        print('Acquiring images...')
+
+        #  Retrieve device serial number for filename
+        #
+        #  *** NOTES ***
+        #  The device serial number is retrieved in order to keep cameras from
+        #  overwriting one another. Grabbing image IDs could also accomplish
+        #  this.
+        device_serial_number = ''
+        node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+        if PySpin.IsAvailable(node_device_serial_number) and PySpin.IsReadable(node_device_serial_number):
+            device_serial_number = node_device_serial_number.GetValue()
+            print('Device serial number retrieved as %s...' % device_serial_number)
+
+        # close program
+        print('Press enter to close the program..')
+
+        # Retrieve and display images
+        while(True):
+            try:
+                #  Retrieve next received image
+                #
+                #  *** NOTES ***
+                #  Capturing an image houses images on the camera buffer. Trying
+                #  to capture an image that does not exist will hang the camera.
+                #
+                #  *** LATER ***
+                #  Once an image from the buffer is saved and/or no longer
+                #  needed, the image must be released in order to keep the
+                #  buffer from filling up.
+                image_result = cam.GetNextImage()
+                
+                #  Ensure image completion
+                #
+                #  *** NOTES ***
+                #  Images can easily be checked for completion. This should be
+                #  done whenever a complete image is expected or required.
+                #  Further, check image status for a little more insight into
+                #  why an image is incomplete.
+                if image_result.IsIncomplete():
+                    print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+                else:
+                    # Figure(1) is default so you can omit this line. figure (0) will create new window every time program hit this line
+                    fig = plt.figure(1)
+
+                try:
+                    # Getting the image data as a numpy array
+                    image_data = image_result.GetNDArray()
+
+                    # Draws an image on the current figure.
+                    plt.imshow(image_data, cmap='gray')
+
+                    plt.pause(0.001)
+
+                    # Clear current reference of a figure. This will improve display speed significantly
+                    plt.clf()
+
+                    # If user presses enter, close the program
+                    if keyboard.is_pressed('ENTER'):
+                        print('Program is closing...')
+
+                        # close the gui
+                        plt.close('all')
+                        input('Done! Press Enter to exit...')
+                        return False
+                        
+                except PySpin.SpinnakerException:
+                    raise
+
+                #  Release image
+                #
+                #  *** NOTES ***
+                #  Images retrieved directly from the camera (i.e. non-converted
+                #  images) need to be released in order to keep from filling the
+                #  buffer.
+                image_result.Release()
+
+            except PySpin.SpinnakerException:
+                raise
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return result
+
+
+def run_single_camera(cam):
+    """
+    This function acts as the body of the example; please see NodeMapInfo example
+    for more in-depth comments on setting up cameras.
+
+    :param cam: Camera to run on.
+    :type cam: CameraPtr
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+
+        nodemap_tldevice = cam.GetTLDeviceNodeMap()
+
+        # Initialize camera
+        cam.Init()
+
+        # Retrieve GenICam nodemap
+        nodemap = cam.GetNodeMap()
+
+        # Acquire images
+        result &= acquire_and_display_images(cam, nodemap, nodemap_tldevice)
+
+        # Deinitialize camera
+        cam.DeInit()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
+
+
+def main():
+    """
+    Example entry point; notice the volume of data that the logging event handler
+    prints out on debug despite the fact that very little really happens in this
+    example. Because of this, it may be better to have the logger set to lower
+    level in order to provide a more concise, focused log.
+
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+
+    # Since this application saves images in the current folder
+    # we must ensure that we have permission to write to this folder.
+    # If we do not have permission, fail right away.
+    try:
+        test_file = open('test.txt', 'w+')
+    except IOError:
+        print('Unable to write to current directory. Please check permissions.')
+        input('Press Enter to exit...')
+        return False
+
+    test_file.close()
+    os.remove(test_file.name)
+
+    result = True
+
+    # Retrieve singleton reference to system object
+    system = PySpin.System.GetInstance()
+
+    # Get current library version
+    version = system.GetLibraryVersion()
+    print('Library version: %d.%d.%d.%d' % (version.major, version.minor, version.type, version.build))
+
+    # Retrieve list of cameras from the system
+    cam_list = system.GetCameras()
+
+    num_cameras = cam_list.GetSize()
+
+    print('Number of cameras detected: %d' % num_cameras)
+
+    # Finish if there are no cameras
+    if num_cameras == 0:
+
+        # Clear camera list before releasing system
+        cam_list.Clear()
+
+        # Release system instance
+        system.ReleaseInstance()
+
+        print('Not enough cameras!')
+        input('Done! Press Enter to exit...')
+        return False
+
+    # Run example on each camera
+    for i, cam in enumerate(cam_list):
+
+        print('Running example for camera %d...' % i)
+
+        result &= run_single_camera(cam)
+        print('Camera %d example complete... \n' % i)
+
+    # Release reference to camera
+    # NOTE: Unlike the C++ examples, we cannot rely on pointer objects being automatically
+    # cleaned up when going out of scope.
+    # The usage of del is preferred to assigning the variable to None.
+    del cam
+
+    # Clear camera list before releasing system
+    cam_list.Clear()
+
+    # Release system instance
+    system.ReleaseInstance()
+
+    input('Done! Press Enter to exit...')
+    return result
+
+
+if __name__ == '__main__':
+    main()
