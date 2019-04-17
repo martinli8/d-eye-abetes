59b407b4edcb987cc5c97beb48c96e4dd860de20
martinli8 <31770777+martinli8@users.noreply.github.com>
2019-04-16 22:00:06 -0400

    Update led_on.py
    edited LED pins

--- led_on.py
+++ led_on.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
 GPIO.setup(23.GPIO.OUT)
 print "LED on"
-GPIO.output(17,GPIO.HIGH)
+GPIO.output(24,GPIO.HIGH)
 GPIO.output(23,GPIO.HIGH)
